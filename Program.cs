c909a898e522f00924fabd5842e338c652a3d0a2
Esmanur15 <esmanurdursun.megayazilim@outlook.com>
2021-09-22 15:47:57 +0300

    kodun genel yapısı oluşturuldu.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace bubble_sort_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dizi = {8,22,5,14,4,32};
+
+            for (int adim = 1; adim < 6; adim++)
+            {
+                for (int j = 0; j < 6-adim; j++)
+                {
+                    if (dizi[j] > dizi[j+1])
+                    {
+                        int araciEleman=dizi[j];
+                        dizi[j] = dizi[j+1];
+                        dizi[j+1] = araciEleman;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 6; i++)
+            {
+                Console.WriteLine(dizi[i]);
+            }
+        }
+    }
+}
